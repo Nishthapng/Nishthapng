46b273e748f0dce76d045b5f187f289d5477659e
Nishthapng <77435268+Nishthapng@users.noreply.github.com>
2022-07-07 15:15:51 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -14,17 +14,22 @@ function modelLoaded(){
     console.log('posenet i intialized');
 }
 function gotPoses(results){
-if(results.length > 0)
-{
-    console.log(results);
-    leftwristX=results[0].pose.leftWrist.x;
+    if(results.length > 0)
+    {
+        console.log(results);
+        noseX= results[0].pose.nose.x;
+        noseY= results[0].pose.nose.y;
+        console.log("noseX= "+noseX+"noseY= "+noseY);
+        leftwristX=results[0].pose.leftWrist.x;
         rightwristX=results[0].pose.rightWrist.x;
-}
+        difference=floor(leftwristX - rightwristX);
+        console.log("leftwristX= "+leftwristX+"rightwristX= "+rightwristX+"difference= "+difference);
+    }
 }
 function draw(){
     background('#6C91C2');
     textsize(10);
     fill('#FFE787');
-    text('best',50,400);
+    text('best',50,400,difference);
 
 }
