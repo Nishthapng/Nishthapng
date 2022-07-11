1fe01b52e5285c5777f32fbbf2a4658929c66b32
Nishthapng <77435268+Nishthapng@users.noreply.github.com>
2022-07-11 15:08:14 +0530

    Update main.js

--- main.js
+++ main.js
@@ -26,10 +26,4 @@ function gotPoses(results){
         console.log("leftwristX= "+leftwristX+"rightwristX= "+rightwristX+"difference= "+difference);
     }
 }
-function draw(){
-    background('#6C91C2');
-    textsize(10);
-    fill('#FFE787');
-    text('best',50,400,difference);
-
-}
+function draw() { background('#6C91C2'); document.getElementById("font_size").innerHTML = "Font size of the text will be = " + difference +"px"; textSize(difference); fill('#FFE787'); text('Best', 50, 400); }
